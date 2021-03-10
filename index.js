ea6c6f20ad4462aabb26e579e67a3856a28ea73c
Shraboni123 <praptishraboni1@gmail.com>
2021-03-10 12:54:47 +0600

    ui

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Calendar from './src/calendar/Calendar'
+import Calendar from './src/calendar/Calendar';
+import Eventlist from './src/Event/Eventlist';
 AppRegistry.registerComponent(appName, () => Calendar);
---
+++ src/Event/Eventlist.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { List, ListItem, Left, Body, Content ,Header, Container} from 'native-base';
+// import {event} from '../sample/data/Event'
+
+export default class extends React.Component {
+    
+    render(){
+        // const eventList=[
+        //     {
+        //       eventId: 1,
+        //       title:"Mountain Hiking" ,
+        //       summary: "",
+        //       tag:["adventurous"]
+        //     },
+        //     {
+        //       eventId: 2,
+        //       title:"Art Exibition" ,
+        //       summary: "",
+        //       tag:["art"]
+        //     },
+        //     {
+        //       eventId: 3,
+        //       title:"Research paper on AI" ,
+        //       summary: "",
+        //       tag:["science"]
+        //     },
+        //     {
+        //       eventId: 4,
+        //       title:"Women's Voices For Change - Redefining Life After forty" ,
+        //       summary: "",
+        //       tag:["empowerment"]
+        //     }
+        //   ]
+          
+        return (
+            <Container>
+                <Content>
+                    <Header/>
+                    <Body>
+
+                    </Body>
+                </Content>
+            </Container>
+            // <View
+            //   style={{
+            //     flex: 1,
+            //     justifyContent: "center",
+            //     alignItems: "center"
+            //   }}>
+            //   <Text>Hello world</Text>
+            // </View>
+          )
+        }
+        
+    }
+  
