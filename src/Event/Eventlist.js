bbcd12d8f14df12887afd08dfc652cb62e080421
Shraboni123 <praptishraboni1@gmail.com>
2021-03-10 16:35:21 +0600

    add event in the calendar from list

--- src/Event/Eventlist.js
+++ src/Event/Eventlist.js
@@ -1,18 +1,112 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { List, ListItem, Left, Body, Content, Header, Container, Label, Button, Right } from 'native-base';
 import { event } from '../../sample/data/Event'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCalendarPlus, faSignInAlt, faSignOutAlt, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarPlus, faThumbsDown, faThumbsUp, faSignInAlt,faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { listCalendar, createEvent, listEvents, createCalendar } from '../calendar/google-calendar.api'
+import { configureGoogleSignIn, getCurrentUser, signIn, signOut } from '../calendar/google-auth'
 
 export default class extends React.Component {
+    state = {
+        loggedIn: false,
+        accessToken: '',
+
+
+    }
+    componentDidMount = async () => {
+        configureGoogleSignIn()
+        let response = await getCurrentUser();
+        if (response.success) {
+            this.setState({ accessToken: response.success.accessToken, loggedIn: true })
+        }
+        else {
+            this.setState({ loggedIn: false })
+        }
+    }
+    handleSignOut = async () => {
+        let response = await signOut()
+        if (response.success) {
+            this.setState({ loggedIn: false })
+        }
+    }
+
+    handleSignIn = async () => {
+        console.log("enter")
+        let response = await signIn()
+        if (response.success) {
+            this.setState({ loggedIn: true, accessToken: response.success.accessToken })
+        }
+    }
+    handleCreateEvent = async (singleEvent) => {
+        console.log(singleEvent)
+        const { accessToken } = this.state
+        const calendarId = "9jafcfgfbo0vj1p38sr0utpd5g@group.calendar.google.com"
+        const event = {
+            'summary': singleEvent.title,
+            "end": {
+                "dateTime": "2021-02-04T09:00:00.000Z",
+                "timeZone": "BST" // 'America/Los_Angeles'
+            },
+            "start": {
+                "dateTime": "2021-02-04T07:00:00.000Z",
+                "timeZone": "BST" // 'America/Los_Angeles'
+            },
+            'description': singleEvent.summary
+        }
+        console.log("event", event)
+        let result = await createEvent(accessToken, calendarId, event)
+        console.log("Event response ", result)
+    }
 
     render() {
         return (
             <Container>
                 <Content>
-                    <Header />
+                    <Header>
+                        {
+                            this.state.loggedIn == false ?
+                                <View >
+                                    <TouchableOpacity
+                                        style={{ marginTop: 15, marginLeft: 250 }}
+                                        onPress={() => { this.handleSignIn() }} >
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <FontAwesomeIcon icon={faSignInAlt} style={{ color: 'white', marginTop: 5 }} size={20} />
+
+                                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', marginLeft: 5, marginBottom: 5 }}>Login</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                                :
+                                <View style={{ marginTop: 10, width: 100, marginLeft: 250 }} >
+                                    <TouchableOpacity
+                                        onPress={() => { this.handleSignOut() }}
+                                    >
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <FontAwesomeIcon icon={faSignOutAlt} style={{ color: 'white', marginTop: 5, marginLeft: 10 }} size={20} />
+
+                                            <Text style={{ fontWeight: 'bold', color: 'white', marginLeft: 5, marginBottom: 5, fontSize: 20, marginRight: 10 }}>Logout</Text>
+                                        </View>
+
+                                    </TouchableOpacity>
+                                </View>
+                        }
+
+
+                    </Header>
                     {
+                        this.state.loggedIn == false ?
+                            <View>
+                                <Text style={{ fontSize: 20, fontWeight: "bold" }}>You need to LogIn</Text>
+                            </View>
+                            :
+                            <View>
+
+                            </View>
+                    }
+                    
+
+                    {  this.state.loggedIn &&
                         event.map((singleEvent, index) => {
 
                             return (
@@ -21,15 +115,15 @@ export default class extends React.Component {
                                     <ListItem selected>
                                         <Left>
                                             <View style={{ flexDirection: "column" }}>
-                                                    <Label style={{ color: "red" }}>{singleEvent.title}</Label>
+                                                <Label style={{ color: "red" }}>{singleEvent.title}</Label>
 
                                                 <Text style={{ color: "black" }}>{singleEvent.summary}</Text>
-                                                <View style={{ flexDirection: 'row',marginTop:10 }}>
-                                                    <Button   success style={{  width: 90,marginRight:20 }}><Text style={{ color:'white', margin: 24 }}>Brave</Text></Button>
-                                                    <Button  success  style={{ width: 90 }}><Text style={{ color :'white',margin: 10 }}>Ambitious</Text></Button>
+                                                <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                                    <Button success style={{ width: 90, marginRight: 20 }}><Text style={{ color: 'white', margin: 24 }}>Brave</Text></Button>
+                                                    <Button success style={{ width: 90 }}><Text style={{ color: 'white', margin: 10 }}>Ambitious</Text></Button>
 
                                                 </View>
-                                                <View style={{ flexDirection: 'row',marginTop:10 }}>
+                                                <View style={{ flexDirection: 'row', marginTop: 10 }}>
                                                     <Button transparent style={{ marginLeft: 20 }}>
                                                         {/* <Text style={{color:"white",margin:10}}>Like</Text> */}
                                                         <FontAwesomeIcon icon={faThumbsUp} style={{ color: '#00008b', }} size={26} />
@@ -50,9 +144,11 @@ export default class extends React.Component {
                                         </Left>
 
                                         <Right>
-                                            <Button transparent style=
-                                                {{ width: 50, marginBottom: 140 }}>
-                                                <FontAwesomeIcon icon={faCalendarPlus} style={{ color:'#00008b' }} size={24} />
+                                            <Button transparent style={{ width: 50, marginBottom: 140 }}
+                                                onPress={() => { this.handleCreateEvent(singleEvent) }} >
+
+
+                                                <FontAwesomeIcon icon={faCalendarPlus} style={{ color: '#00008b' }} size={24} />
 
                                                 {/* <Text style={{margin:10,color:"white"}}>Add</Text> */}
                                             </Button>
