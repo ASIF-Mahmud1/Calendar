ef0731fefe61d25b282e0607b16370fa03e94ae2
Shraboni123 <praptishraboni1@gmail.com>
2021-03-18 11:59:01 +0600

    index file run properly

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Calendar from './src/calendar/Calendar';
 import Eventlist from './src/Event/Eventlist';
-AppRegistry.registerComponent(appName, () => Eventlist);
+import Stack from './src/Event/index'
+AppRegistry.registerComponent(appName, () => Stack);
